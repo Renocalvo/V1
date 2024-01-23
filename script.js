e130acd2c3c959d4fcb723a180a1b8fcbc807fe9
Saya__IB <indrabayu210903@gmail.com>
2024-01-23 21:22:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function downloadCV() {
   
   // Menetapkan atribut unduhan dan href
   tempLink.setAttribute('href', '/Contoh-resume.pdf'); // Gantilah dengan path yang sesuai
-  tempLink.setAttribute('download', 'Contoh-resume.pdf'); // Gantilah dengan nama file yang sesuai
+  tempLink.setAttribute('download', '/Contoh-resume.pdf'); // Gantilah dengan nama file yang sesuai
 
   // Menambahkan elemen ke dokumen
   document.body.appendChild(tempLink);
